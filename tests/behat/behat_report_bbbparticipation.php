20d7cdea38e4e979cb41314d9b8dcc0327394111
Angela Baier <angela.baier@univie.ac.at>
2021-04-15 09:44:56 +0200

    behat

--- tests/behat/behat_report_bbbparticipation.php
+++ tests/behat/behat_report_bbbparticipation.php
@@ -22,10 +22,6 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-// NOTE: no MOODLE_INTERNAL test here, this file may be required by behat before including /config.php.
-
-require_once(__DIR__ . '/../../../../../../lib/behat/behat_base.php');
-
 /**
  * Steps definitions related with the bbbaprticipation.
  *
